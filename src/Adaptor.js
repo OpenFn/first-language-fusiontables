7ab92b1f9c0a595e73adbda66134847e9eb0ccba
Abhishek Pandit <peppypanditji@gmail.com>
2016-05-10 18:11:50 +0530

    Updated client.js and adaptor.js with Google oAuth format

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -39,25 +39,28 @@ export function execute(...operations) {
 export function row(rowData) {
 
   return state => {
-    const body = expandReferences(rowData)(state);
-
+    const body = INSERT INTO <table_id> (<column_name> {, <column_name>}*) VALUES (<value> {, <value>}*){ {;INSERT INTO <table_id> (<column_name> {, <column_name>}*) VALUES (<value> {, <value>}*)}* ;}
     const { username, password, apiUrl } = state.configuration;
 
     //same
     // where are these being entered from- username, password, apiURL
 
     const url = resolveUrl(apiUrl + '/', 'api/events')
-    // to figure out where to point to actual HTTP endpoint that we're going to poste
+    // to figure out where to point to actual HTTP endpoint that we're going to post
 
     console.log("Posting Row:");
     console.log(body)
 
-    return post({ username, password, body, url })
+    return post({ username, password, body, url, clientID, scope})
     .then((result) => {
       console.log("Success:", result);
       return { ...state, references: [ result, ...state.references ] }
     })
-
+//Could you explain this last return statement? Doesn't make sense to me. 
   }
+
+// "INSERT INTO "+table_id+(fields)+" VALUES "+(dataValues)
+// Values have to each be in Quotes
+
 }
 // Will create and authorize JSON doc with user name and doc and post it to api/url
